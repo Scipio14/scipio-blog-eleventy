f68e097f6199d4d863534f2f799fc4159098dfdd
@Scipio14 <fma.abogado@gmail.com>
2024-06-15 08:12:26 -0600

    Creación del layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig){
+    //Archivos statáticos que se requieren compilar a producción
+    eleventyConfig.addPassthroughCopy("code/css");
+    eleventyConfig.addPassthroughCopy("code/js");
+    eleventyConfig.addPassthroughCopy("code/img");
     return{
         dir:{
             input:"code",
